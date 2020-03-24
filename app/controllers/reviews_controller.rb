41cbb43fd462fc63ce35d5f2d93e4ff04cb2ba42
Gabe Lafontant <glafonta@gmail.com>
2020-03-24 13:14:22 -0400

    ui for writing reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,28 @@
 class ReviewsController < ApplicationController
+  before_action :set_movie
+
+  def new
+    @review = @movie.reviews.new
+  end
+
+  def create
+    @review = @movie.reviews.create(review_params)
+    return render :new if @review.errors.any?
+    redirect_to movie_reviews_path @movie, notice: "Thank you for your Review!"
+  end
+
   def index
-    @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
   end
+
+  private
+
+    def review_params
+      params.require(:review).permit(:name, :comment, :stars)
+    end
+
+    def set_movie
+      return @movie if defined? @movie
+      @movie = Movie.find(params[:movie_id])
+    end
 end
