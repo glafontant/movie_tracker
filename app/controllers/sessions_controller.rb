daff9bb7bf5cf5e2c03223e0b8782c39aeb36549
Gabe Lafontant <glafonta@gmail.com>
2020-03-29 13:15:46 -0400

    authenticate either by username or email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
+    user = User.find_by_email(params[:email_or_username]) ||
+          User.find_by_username(params[:email_or_username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to user, notice: "Welcome Back, #{user.name}!"
