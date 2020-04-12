83df80fad80d09bd7e145d3fb04aade0cea553dc
Gabe Lafontant <glafonta@gmail.com>
2020-04-12 17:13:10 -0400

    allow admin users to upload images in prod via active storage

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -41,7 +41,7 @@ class MoviesController < ApplicationController
     def movie_params
       params.require(:movie).
         permit(:title, :rating, :total_gross, :description, :released_on,
-               :director, :duration, :image_file_name, genre_ids: [])
+               :director, :duration, :main_image, genre_ids: [])
     end
 
     def movies_filter
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,18 +8,18 @@ class Movie < ApplicationRecord
     has_many :characterizations, dependent: :destroy
     has_many :genres, through: :characterizations
 
+    has_one_attached :main_image
+
     RATINGS = %w(G PG PG-13 R NC-17)
     
     validates :title, presence: true, uniqueness: true
     validates :released_on, presence: true
     validates :description, length: { minimum: 25 }
     validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
-    validates :image_file_name, format: {
-      with: /\w+\.(jpg|png)\z/i,
-      message: "must be a JPG or PNG image"
-    }
     validates :rating, inclusion: { in: RATINGS,
       message: "is not a valid rating"}
+
+    validate :acceptable_image
 
     scope :released, -> { where("released_on < ?", Time.now).order(released_on: :desc) }
     scope :hits, -> { where("total_gross >= ?", 300000000).order(total_gross: :desc) }
@@ -55,5 +55,18 @@ class Movie < ApplicationRecord
 
     def set_slug
       self.slug = title.parameterize
+    end
+
+    def acceptable_image
+      return unless main_image.attached?
+
+      unless main_image.blob.byte_size <= 1.megabyte
+        errors.add(:main_image, "is too big")
+      end
+
+      acceptable_types = ["image/jpeg", "image/png"]
+      unless acceptable_types.include?(main_image.blob.content_type)
+        errors.add(:main_image, "must be a JPEG or PNG")
+      end
     end
 end
